078673405044ebad968e49f8f7c30aedc1ce7433
Xavier Simó <xsimov@gmail.com>
2016-11-23 18:08:22 +0100

    Added tests for DataImporter#new

--- spec/data_importer_spec.rb
+++ spec/data_importer_spec.rb
@@ -1,6 +1,26 @@
 require_relative '../lib/data_importer'
 
 RSpec.describe DataImporter do
+  describe '#new' do
+    it 'raises ArgumentError if no argument is given' do
+      expect { DataImporter.new() }.to raise_exception(ArgumentError)
+    end
+
+    it 'raises an error if the argument is not a valid path' do
+      expect { DataImporter.new('not/a/valid/path') }.to(
+        raise_exception(Errno::ENOENT)
+      )
+    end
+
+    it 'calls Nokogiri::XML with the given path' do
+      allow(Nokogiri).to receive(:XML)
+      DataImporter.new('spec/fixtures/empty.xml')
+      expect(Nokogiri).to have_received(:XML) do |arg|
+        expect(arg.path).to eq('spec/fixtures/empty.xml')
+      end
+    end
+  end
+
   describe '#get_objects' do
     context 'with an empty xml' do
       it 'returns an array' do
